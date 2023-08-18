8ab6612ceacc576bb77abe59ab9c137d9d66cb22
Aaldn <aaldn.dev@gmail.com>
2023-08-18 22:34:13 +0200

    perf: import google fonts in assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/src/app.dart';
 import 'package:portfolio/src/localization/app_localizations.dart';
 import 'package:url_strategy/url_strategy.dart';
@@ -14,6 +15,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   setPathUrlStrategy();
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(
     ProviderScope(
       child: EasyLocalization(
