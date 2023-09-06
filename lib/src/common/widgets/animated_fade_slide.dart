d90535544335478d3e2816319d544ad742458eb3
Aaldn <aaldn.dev@gmail.com>
2023-09-06 22:43:01 +0200

    feat: fade slide animation

---
+++ lib/src/common/widgets/animated_fade_slide.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class AnimatedFadeSlide extends HookConsumerWidget {
+  const AnimatedFadeSlide({
+    super.key,
+    this.delay = const Duration(milliseconds: 0),
+    this.duration = const Duration(milliseconds: 500),
+    this.offset = const Offset(100, -64),
+    required this.child,
+  });
+
+  final Duration delay;
+  final Duration duration;
+  final Offset offset;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useAnimationController(duration: duration);
+    final dxAnimation = useAnimation(
+      Tween(begin: offset.dx, end: 0.0).animate(controller),
+    );
+    final dyAnimation = useAnimation(
+      Tween(begin: offset.dy, end: 0.0).animate(controller),
+    );
+
+    useEffect(() {
+      Future.delayed(delay, () {
+        if (context.mounted) {
+          controller.forward();
+        }
+      });
+      return controller.dispose;
+    }, [controller]);
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (_, child) {
+        return Opacity(
+          opacity: controller.value,
+          child: Transform.translate(
+            offset: Offset(dxAnimation, dyAnimation),
+            child: child,
+          ),
+        );
+      },
+      child: child,
+    );
+  }
+}
