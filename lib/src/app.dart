d3d98c3a89a8fd4a7d4f32932e34ba6e7858818c
Aaldn <aaldn.dev@gmail.com>
2023-09-08 14:48:17 +0200

    refactor: remove precacheImage

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,29 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portfolio/src/constants/themes.dart' as themes;
-import 'package:portfolio/src/constants/transparent_image.dart';
 import 'package:portfolio/src/features/main/presentation/main_section.dart';
 import 'package:portfolio/src/features/main/provider/dark_mode_controller.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:portfolio/src/features/project/data/project_repository.dart';
 import 'package:portfolio/src/localization/generated/locale_keys.g.dart';
 
-class MyApp extends ConsumerStatefulWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  ConsumerState<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends ConsumerState<MyApp> {
-  @override
-  void didChangeDependencies() {
-    _preCacheAllImages();
-    super.didChangeDependencies();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       onGenerateTitle: (_) => tr(LocaleKeys.name),
@@ -38,19 +25,5 @@ class _MyAppState extends ConsumerState<MyApp> {
           ),
       home: const MainSection(),
     );
-  }
-
-  void _preCacheAllImages() {
-    // Projects
-    final projects = ref.read(projectRepositoryProvider).getProjects();
-    for (var project in projects) {
-      final projectScreenshotUrl = project.screenshotUrl;
-      if (projectScreenshotUrl != null) {
-        final projectImage = AssetImage(projectScreenshotUrl);
-        precacheImage(projectImage, context);
-      }
-    }
-    // Placeholder
-    precacheImage(MemoryImage(transparentImage), context);
   }
 }
