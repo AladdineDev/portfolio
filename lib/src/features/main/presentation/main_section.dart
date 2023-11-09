a912ce53d33078666ef7ce939ea4a69930d03601
Aaldn <aaldn.dev@gmail.com>
2023-11-09 18:32:45 +0100

    fix: bottom banner text selection and multiline

--- lib/src/features/main/presentation/main_section.dart
+++ lib/src/features/main/presentation/main_section.dart
@@ -5,7 +5,6 @@ import 'package:portfolio/src/features/main/presentation/main_section_tablet.dar
 import 'package:portfolio/src/features/main/presentation/widgets/bottom_banner.dart';
 import 'package:portfolio/src/features/main/presentation/widgets/end_drawer.dart';
 import 'package:portfolio/src/features/main/presentation/widgets/safe_area.dart';
-import 'package:portfolio/src/features/main/provider/scroll_controller.dart';
 import 'package:portfolio/src/common/widgets/responsive.dart';
 
 class MainSection extends ConsumerStatefulWidget {
@@ -16,37 +15,23 @@ class MainSection extends ConsumerStatefulWidget {
 }
 
 class _MainSectionState extends ConsumerState<MainSection> {
-  double _bottomBannerHeight = 0;
-
-  void _displayBottomBanner() {
-    final scrollController = ref.watch(scrollControllerProvider);
-    final scrollPosition = scrollController.position;
-    if (scrollPosition.pixels >= scrollPosition.maxScrollExtent - 64) {
-      setState(() => _bottomBannerHeight = 48);
-    } else {
-      setState(() => _bottomBannerHeight = 0);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    ref.watch(scrollControllerProvider).addListener(_displayBottomBanner);
-
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.secondary,
       endDrawer: const MySafeArea(
         child: EndDrawer(),
       ),
-      body: MySafeArea(
+      body: const MySafeArea(
         child: Stack(
           children: [
-            const Responsive(
+            Responsive(
               desktop: MainDesktop(),
               tablet: MainTablet(),
             ),
             Align(
               alignment: Alignment.bottomCenter,
-              child: BottomBanner(bannerHeight: _bottomBannerHeight),
+              child: BottomBanner(),
             )
           ],
         ),
--- lib/src/features/main/presentation/widgets/bottom_banner.dart
+++ lib/src/features/main/presentation/widgets/bottom_banner.dart
@@ -1,42 +1,85 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import 'package:portfolio/src/common/widgets/link.dart';
 import 'package:portfolio/src/common/widgets/selection_area.dart';
+import 'package:portfolio/src/features/main/provider/scroll_controller.dart';
 import 'package:portfolio/src/localization/generated/locale_keys.g.dart';
+import 'package:portfolio/src/utils/launch_url_helper.dart';
+import 'package:portfolio/src/utils/scaffold_messenger_helper.dart';
 
-class BottomBanner extends ConsumerWidget {
-  const BottomBanner({super.key, this.bannerHeight = 48});
-
-  final double bannerHeight;
+class BottomBanner extends ConsumerStatefulWidget {
+  const BottomBanner({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<BottomBanner> createState() => _BottomBannerState();
+}
+
+class _BottomBannerState extends ConsumerState<BottomBanner> {
+  double _height = 0;
+
+  void _displayBottomBanner() {
+    final scrollController = ref.watch(scrollControllerProvider);
+    final scrollPosition = scrollController.position;
+    if (scrollPosition.pixels >= scrollPosition.maxScrollExtent - 64) {
+      setState(() => _height = 56);
+    } else {
+      setState(() => _height = 0);
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    ref.watch(scrollControllerProvider).addListener(_displayBottomBanner);
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: const Duration(milliseconds: 500),
       width: double.maxFinite,
-      height: bannerHeight,
+      height: _height,
       color: Theme.of(context).colorScheme.secondary,
       child: DefaultTextStyle(
         style: Theme.of(context).textTheme.titleSmall!,
         child: MySelectionArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "${tr(LocaleKeys.bottomBanner_message)} ",
-              ),
-              LinkWidget(
-                url: tr(LocaleKeys.bottomBanner_linkUrl),
-                displayLink: tr(LocaleKeys.bottomBanner_displayLink),
-                underlined: true,
-                hoverColor: Theme.of(context).colorScheme.onSurface,
-              ),
-            ],
+          child: Text.rich(
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: "${tr(LocaleKeys.bottomBanner_message)} ",
+                ),
+                TextSpan(
+                  style: const TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      _onTap(
+                        context,
+                        url: tr(LocaleKeys.bottomBanner_linkUrl),
+                      );
+                    },
+                  text: tr(LocaleKeys.bottomBanner_displayLink),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _onTap(BuildContext context, {required String url}) async {
+    try {
+      await LaunchUrlHelper.launchURL(url);
+    } catch (e) {
+      if (!context.mounted) return;
+      ScaffoldMessengerHelper.showLaunchUrlError(context, url: url);
+    }
   }
 }
