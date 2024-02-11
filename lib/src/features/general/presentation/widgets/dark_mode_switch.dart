5131505c5a4e0d5f324bfcac6267ca13c3268e0a
Aaldn <aaldn.dev@gmail.com>
2024-02-11 16:48:57 +0100

    feat: add animated dark mode switch

--- lib/src/features/general/presentation/widgets/dark_mode_switch.dart
+++ lib/src/features/general/presentation/widgets/dark_mode_switch.dart
@@ -1,3 +1,4 @@
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portfolio/src/features/general/provider/dark_mode_controller.dart';
@@ -7,18 +8,39 @@ class DarkModeSwitch extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const Icon(Icons.wb_sunny_outlined),
-        Switch(
-          value: _getDarkMode(ref),
-          onChanged: (_) {
-            ref.read(darkModeProvider.notifier).updateTheme();
-          },
+    return SelectionContainer.disabled(
+      child: AnimatedToggleSwitch<bool>.dual(
+        current: _getDarkMode(ref),
+        onChanged: (_) => ref.read(darkModeProvider.notifier).updateTheme(),
+        first: false,
+        second: true,
+        spacing: 8,
+        height: 36,
+        indicatorSize: const Size.square(32),
+        style: ToggleStyle(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          borderColor: Colors.transparent,
         ),
-        Icon(_getDarkMode(ref) ? Icons.mode_night : Icons.mode_night_outlined),
-      ],
+        styleBuilder: (_) => ToggleStyle(
+          indicatorColor: _getDarkMode(ref)
+              ? Theme.of(context).colorScheme.onPrimary
+              : Theme.of(context).switchTheme.thumbColor?.resolve({}),
+        ),
+        iconBuilder: (darkMode) {
+          return _buildSwitchIcon(
+            ref: ref,
+            context: context,
+            darkMode: darkMode,
+          );
+        },
+        textBuilder: (darkMode) {
+          return _buildSwitchIcon(
+            ref: ref,
+            context: context,
+            darkMode: !darkMode,
+          );
+        },
+      ),
     );
   }
 
@@ -27,5 +49,28 @@ class DarkModeSwitch extends ConsumerWidget {
           data: (darkMode) => darkMode,
           orElse: () => ThemeMode.system == ThemeMode.dark,
         );
+  }
+
+  Icon _buildSwitchIcon({
+    required WidgetRef ref,
+    required BuildContext context,
+    required bool darkMode,
+  }) {
+    if (darkMode) {
+      if (_getDarkMode(ref)) {
+        return Icon(
+          Icons.mode_night_outlined,
+          color: Theme.of(context).colorScheme.onInverseSurface,
+        );
+      }
+      return const Icon(Icons.mode_night_outlined);
+    }
+    if (!_getDarkMode(ref)) {
+      return Icon(
+        Icons.wb_sunny_outlined,
+        color: Theme.of(context).colorScheme.onInverseSurface,
+      );
+    }
+    return const Icon(Icons.wb_sunny_outlined);
   }
 }
