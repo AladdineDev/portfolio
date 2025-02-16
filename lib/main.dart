c774e3dc2822b590f1c5d54c64600fbc40feeb27
AladdineDev <aaldn.dev@gmail.com>
2025-02-16 13:59:50 +0100

    build: upgrade flutter version and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:portfolio/src/app.dart';
 import 'package:portfolio/src/app_startup.dart';
 import 'package:portfolio/src/localization/app_localizations.dart';
 import 'package:portfolio/src/localization/locale_controller.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   EasyLocalization.logger.enableBuildModes = [];
-  setPathUrlStrategy();
+  usePathUrlStrategy();
   GoogleFonts.config.allowRuntimeFetching = false;
   final supportedLocales = await AppLocalizations.supportedLocales();
   runApp(
