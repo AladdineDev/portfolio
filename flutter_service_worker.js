64738354ca182a74bb5cece6d075c1996f97c8d2
AladdineDev <AladdineDev@users.noreply.github.com>
2025-03-10 21:41:15 +0000

    deploy: fe19a9b91a7054a98668424e855b6bee87d37ab2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "3c00bfb5fa6c03e5efff656e896ed43c",
-"main.dart.js": "d2d8f3296f78480109009577c3dff69a",
+"main.dart.js": "ec01d5b1f539d591f7d0e1f409972d2f",
 "version.json": "009c9e65172e010890f7f65fde438006",
 "assets/assets/images/gsb-rv-visiteur-serveur.png": "aa279e9a72f05f8041d4259f1ffbc640",
 "assets/assets/images/logo.svg": "df08988fdc848d0a0722655527f00dcd",
@@ -40,7 +40,7 @@ const RESOURCES = {"manifest.json": "3c00bfb5fa6c03e5efff656e896ed43c",
 "assets/assets/icons/software-development/graphql.svg": "02a654bb3808332f6191eb39114c874b",
 "assets/assets/icons/flags/france.svg": "5e0abb6d24264435255a8f94c369ac1d",
 "assets/assets/icons/flags/united-states-of-america.svg": "cfe34e9f0aee58b9de91362c139a90bd",
-"assets/assets/translations/fr.json": "d597a275bd34d095dd9ee19a0c58a9dc",
+"assets/assets/translations/fr.json": "813df2e41f0011915dfcf0615c9a9745",
 "assets/assets/translations/en.json": "3076ba22943597466ca8ddacd739efa7",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "5bf08e8ce099dd8da15f71d17342b078",
@@ -78,8 +78,8 @@ const RESOURCES = {"manifest.json": "3c00bfb5fa6c03e5efff656e896ed43c",
 "splash/img/dark-2x.png": "d4298fa1d61c750418bf13f6417375cb",
 "splash/img/dark-4x.png": "7a08e8e8c5e8efbd817575f489ec69e3",
 "splash/img/dark-1x.png": "c6fd8dd64c2826d0c49c91d360f2f270",
-"main.dart.wasm": "b40e208704f3bfc34a76e0d5dfa7011a",
-"flutter_bootstrap.js": "af55343e2fa99b5ef1142e6a750ab186"};
+"main.dart.wasm": "95d38aa8e94ad9ef5b5ca026b0923cc3",
+"flutter_bootstrap.js": "3660f2c9fd9d2e5b9d35ed9afa8a4c15"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
