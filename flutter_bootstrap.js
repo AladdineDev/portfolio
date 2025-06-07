9775a94bc5d31ddaab6714a6c21848d8da8e2f3a
AladdineDev <AladdineDev@users.noreply.github.com>
2025-06-07 23:20:47 +0000

    deploy: 9129604272ceb0796fb4689596df77709fd46c9c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1953925143"
+    serviceWorkerVersion: "379017362"
   }
 });
